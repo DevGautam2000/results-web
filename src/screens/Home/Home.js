69e499aa7e52fc954d4f872fd0770ef6d5e277a4
Gautam <devgautam1231@gmail.com>
2021-06-12 16:44:11 +0530

    fixed connection errors

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Motion from "../../js/Motion";
 import Bar from "../../components/Bar/Bar";
 import Card from "../../components/Card/Card";
