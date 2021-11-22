75cbffa9a5f29280e181bb4328908ac798f82f6b
Gautam Chandra Saha <devgautam1231@gmail.com>
2021-11-22 13:25:03 +0530

    minor change to links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <div>Results Fetched From SMIT results</div>
+      <div>
+        Results Fetched From{" "}
+        <Link
+          style={{ color: "#5454ec" }}
+          onClick={() => window.open("https://result.smtech.in/")}
+        >
+          result.smtech.in
+        </Link>
+      </div>
       <div>NOTE: Developer is not responsible for accuracy of marks</div>
       <div>2021 &copy; Gautam Chandra Saha</div>
     </footer>
