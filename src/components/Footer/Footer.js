33c6b2a7fbf6bb7fd0b7b798fe27983033db1863
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-03-12 23:27:00 +0530

    updated copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
         </a>
       </div>
       <div>NOTE: Developer is not responsible for accuracy of marks</div>
-      <div>2021 &copy; Gautam Chandra Saha</div>
+      <div>{new Date().getFullYear()} &copy; Gautam Chandra Saha</div>
     </footer>
   );
 };
